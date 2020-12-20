d9e783b359437b4cd6eb4421f3052dd33fd0620b
Gareth Coles <gdude2002@gmail.com>
2020-12-20 17:00:27 +0000

    Add descriptions for each command

--- ext-mappings/src/main/kotlin/com/kotlindiscord/kordex/MappingsExtension.kt
+++ ext-mappings/src/main/kotlin/com/kotlindiscord/kordex/MappingsExtension.kt
@@ -43,6 +43,18 @@ class MappingsExtension(bot: ExtensibleBot) : Extension(bot) {
         command {
             name = "class"
 
+            description = "Look up mappings info for a class, given a specific mappings namespace.\n\n" +
+
+                    "**Namespaces:** `mcp`, `mojang`, `yarn`\n" +
+                    "**Yarn channels:** " + YarnChannels.values().joinToString(", ") { "`${it.str}`" } +
+                    "\n\n" +
+
+                    "Instead of specifying a mappings namespace here, you can use the following commands to query " +
+                    "each namespace directly: `mcpc`, `mmc` or `yc`.\n\n" +
+
+                    "For more information or a list of versions for each namespace, you can use the following " +
+                    "commands: `mcp`, `mojang` or `yarn`."
+
             aliases = arrayOf("c")
             signature(::GenericArguments)
 
@@ -61,6 +73,18 @@ class MappingsExtension(bot: ExtensibleBot) : Extension(bot) {
         command {
             name = "field"
 
+            description = "Look up mappings info for a field, given a specific mappings namespace.\n\n" +
+
+                    "**Namespaces:** `mcp`, `mojang`, `yarn`\n" +
+                    "**Yarn channels:** " + YarnChannels.values().joinToString(", ") { "`${it.str}`" } +
+                    "\n\n" +
+
+                    "Instead of specifying a mappings namespace here, you can use the following commands to query " +
+                    "each namespace directly: `mcpf`, `mmf` or `yf`.\n\n" +
+
+                    "For more information or a list of versions for each namespace, you can use the following " +
+                    "commands: `mcp`, `mojang` or `yarn`."
+
             aliases = arrayOf("f")
             signature(::GenericArguments)
 
@@ -78,6 +102,18 @@ class MappingsExtension(bot: ExtensibleBot) : Extension(bot) {
         // Method
         command {
             name = "method"
+
+            description = "Look up mappings info for a method, given a specific mappings namespace.\n\n" +
+
+                    "**Namespaces:** `mcp`, `mojang`, `yarn`\n" +
+                    "**Yarn channels:** " + YarnChannels.values().joinToString(", ") { "`${it.str}`" } +
+                    "\n\n" +
+
+                    "Instead of specifying a mappings namespace here, you can use the following commands to query " +
+                    "each namespace directly: `mcpm`, `mmm` or `ym`.\n\n" +
+
+                    "For more information or a list of versions for each namespace, you can use the following " +
+                    "commands: `mcp`, `mojang` or `yarn`."
 
             aliases = arrayOf(
                 "m",  // Short command name
@@ -109,6 +145,10 @@ class MappingsExtension(bot: ExtensibleBot) : Extension(bot) {
         command {
             name = "mcpc"
 
+            description = "Look up MCP mappings info for a class.\n\n" +
+
+                    "For more information or a list of versions for MCP mappings, you can use the `mcp` command."
+
             signature(::MCPArguments)
 
             action {
@@ -126,6 +166,10 @@ class MappingsExtension(bot: ExtensibleBot) : Extension(bot) {
         command {
             name = "mcpf"
 
+            description = "Look up MCP mappings info for a field.\n\n" +
+
+                    "For more information or a list of versions for MCP mappings, you can use the `mcp` command."
+
             signature(::MCPArguments)
 
             action {
@@ -142,6 +186,10 @@ class MappingsExtension(bot: ExtensibleBot) : Extension(bot) {
         // Method
         command {
             name = "mcpm"
+
+            description = "Look up MCP mappings info for a method.\n\n" +
+
+                    "For more information or a list of versions for MCP mappings, you can use the `mcp` command."
 
             signature(::MCPArguments)
 
@@ -165,6 +213,10 @@ class MappingsExtension(bot: ExtensibleBot) : Extension(bot) {
             name = "mmc"
             aliases = arrayOf("mojc", "mojmapc")
 
+            description = "Look up Mojang mappings info for a class.\n\n" +
+
+                    "For more information or a list of versions for Mojang mappings, you can use the `mojang` command."
+
             signature(::MojangArguments)
 
             action {
@@ -183,6 +235,10 @@ class MappingsExtension(bot: ExtensibleBot) : Extension(bot) {
             name = "mmf"
             aliases = arrayOf("mojf", "mojmapf")
 
+            description = "Look up Mojang mappings info for a field.\n\n" +
+
+                    "For more information or a list of versions for Mojang mappings, you can use the `mojang` command."
+
             signature(::MojangArguments)
 
             action {
@@ -200,6 +256,10 @@ class MappingsExtension(bot: ExtensibleBot) : Extension(bot) {
         command {
             name = "mmm"
             aliases = arrayOf("mojm", "mojmapm")
+
+            description = "Look up Mojang mappings info for a method.\n\n" +
+
+                    "For more information or a list of versions for Mojang mappings, you can use the `mojang` command."
 
             signature(::MojangArguments)
 
@@ -223,6 +283,13 @@ class MappingsExtension(bot: ExtensibleBot) : Extension(bot) {
             name = "yc"
             aliases = arrayOf("yarnc")
 
+            description = "Look up Yarn mappings info for a class.\n\n" +
+
+                    "**Yarn channels:** " + YarnChannels.values().joinToString(", ") { "`${it.str}`" } +
+                    "\n\n" +
+
+                    "For more information or a list of versions for Mojang mappings, you can use the `yarn` command."
+
             signature(::YarnArguments)
 
             action {
@@ -241,6 +308,13 @@ class MappingsExtension(bot: ExtensibleBot) : Extension(bot) {
             name = "yf"
             aliases = arrayOf("yarnf")
 
+            description = "Look up Yarn mappings info for a field.\n\n" +
+
+                    "**Yarn channels:** " + YarnChannels.values().joinToString(", ") { "`${it.str}`" } +
+                    "\n\n" +
+
+                    "For more information or a list of versions for Mojang mappings, you can use the `yarn` command."
+
             signature(::YarnArguments)
 
             action {
@@ -258,6 +332,13 @@ class MappingsExtension(bot: ExtensibleBot) : Extension(bot) {
         command {
             name = "ym"
             aliases = arrayOf("yarnm")
+
+            description = "Look up Yarn mappings info for a method.\n\n" +
+
+                    "**Yarn channels:** " + YarnChannels.values().joinToString(", ") { "`${it.str}`" } +
+                    "\n\n" +
+
+                    "For more information or a list of versions for Mojang mappings, you can use the `yarn` command."
 
             signature(::YarnArguments)
 
@@ -279,6 +360,8 @@ class MappingsExtension(bot: ExtensibleBot) : Extension(bot) {
         // MCP
         command {
             name = "mcp"
+
+            description = "Get information and a list of supported versions for MCP mappings."
 
             action {
                 val defaultVersion = MCPNamespace.getDefaultVersion()
@@ -319,6 +402,9 @@ class MappingsExtension(bot: ExtensibleBot) : Extension(bot) {
         // Mojang
         command {
             name = "mojang"
+            aliases = arrayOf("mojmap")
+
+            description = "Get information and a list of supported versions for Mojang mappings."
 
             action {
                 val defaultVersion = MojangNamespace.getDefaultVersion()
@@ -359,6 +445,8 @@ class MappingsExtension(bot: ExtensibleBot) : Extension(bot) {
         // Yarn
         command {
             name = "yarn"
+
+            description = "Get information and a list of supported versions for Yarn mappings."
 
             action {
                 val defaultVersion = YarnNamespace.getDefaultVersion()
