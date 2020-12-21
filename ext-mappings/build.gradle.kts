a5af0262608e0b996e4aadf3149b736613997eae
Gareth Coles <gdude2002@gmail.com>
2020-12-21 16:32:32 +0000

    Move subprojects block into each module

--- ext-mappings/build.gradle.kts
+++ ext-mappings/build.gradle.kts
@@ -4,3 +4,45 @@ version = "1.0.0-SNAPSHOT"
 dependencies {
     api("me.shedaniel:linkie-core:1.0.43")
 }
+
+val sourceJar = task("sourceJar", Jar::class) {
+    dependsOn(tasks["classes"])
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+tasks.build {
+    this.finalizedBy(sourceJar)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "KotDis"
+
+            url = if (version.toString().contains("SNAPSHOT")) {
+                uri("https://maven.kotlindiscord.com/repository/maven-snapshots/")
+            } else {
+                uri("https://maven.kotlindiscord.com/repository/maven-releases/")
+            }
+
+            credentials {
+                username = project.findProperty("kotdis.user") as String?
+                    ?: System.getenv("KOTLIN_DISCORD_USER")
+
+                password = project.findProperty("kotdis.password") as String?
+                    ?: System.getenv("KOTLIN_DISCORD_PASSWORD")
+            }
+
+            version = version
+        }
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            from(components.getByName("java"))
+
+            artifact(sourceJar)
+        }
+    }
+}
